5be5c17cdc7c5494593e63adb431ebfc24b231f0
AnantLeader <infinityideas26@gmail.com>
2025-06-18 14:45:54 +0100

    new testset

---
+++ cypress/e2e/addToCartTest.cy.js
@@ -0,0 +1,24 @@
+import { homePage } from "../pages/homePage";
+const homePageObj= new homePage()
+import testData from '../fixtures/testData.json'
+
+describe(' test automation',() =>{
+ before(() =>{
+    cy.login(testData.login.username,testData.login.password)
+
+ })
+
+ it('Add to Cart flow',() =>{
+        homePageObj.searchProduct(testData.product.productName)
+        homePageObj.addToCart()
+        homePageObj.verifySuccessMessage().should('contain',testData.message.successMessage).and('contain',testData.product.productName);
+        homePageObj.clearSearchProduct()
+        homePageObj.searchProduct(testData.product.productNameOne)
+        homePageObj.addToCart()
+        homePageObj.verifySuccessMessage().should('contain',testData.message.successMessage1).and('contain',testData.product.productNameOne);
+
+ })
+
+}
+    
+    )
