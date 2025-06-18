dc523d7219d4ff52213a9ae5cc72d8e4e5217653
AnantLeader <infinityideas26@gmail.com>
2025-06-18 21:10:57 +0100

    Slack Integration upfate

--- cypress/pages/homePage.js
+++ cypress/pages/homePage.js
@@ -4,7 +4,7 @@ export class homePage{
     weblocators = {
 
         search_input:'.form-control.input-lg',
-        click_search: '.btn.btn-default.btn-lg1',
+        click_search: '.btn.btn-default.btn-lg',
         product:'img[title="MacBook"]',
       //  productOne:'img[title="iPhone"]',
         addtocart:'Add to Cart',
